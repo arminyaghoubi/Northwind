e4a07c76997e611a8e50c406ba5b24fe6a86b683
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-12-03 10:30:14 +0330

    Create Empty ASP.Net Empty and Config for Redirect to Https

--- Northwind.Web/Program.cs
+++ Northwind.Web/Program.cs
@@ -1,34 +1,18 @@
-namespace Northwind.Web
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapGet("/hello", () => "Hello World!");
+
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddRazorPages();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapRazorPages();
-
-            app.Run();
-        }
-    }
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+
+app.Run();
+
+Console.WriteLine("This executes after the web server has stopped!");
